0e9f3d0f8e41efdbd9b0c4bccfb89a02a51c5502
Admin <admin@Admins-MacBook-Pro.local>
2021-01-07 19:30:01 +0600

    create route and model

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
 
+//connect database
+
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
+
+// Define route
+
+app.use('/api/users', require('./routes/api/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/api/profile'));
+app.use('/api/posts', require('./routes/api/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
