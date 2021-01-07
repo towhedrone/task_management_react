7bc4e28119330606f47d0001a9dd009e8cf47c2b
Admin <admin@Admins-MacBook-Pro.local>
2021-01-07 20:01:52 +0600

    mongodb connected

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.get('/', (req, res) => res.send('API running'));
 
 // Define route
 
-app.use('/api/users', require('./routes/api/api/users'));
+app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/api/profile'));
-app.use('/api/posts', require('./routes/api/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
